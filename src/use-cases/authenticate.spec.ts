45a7a3c5487f40ae4e37b3fbdc6e243f4bbd5bcb
wagnermateus <wagnermateus7@gmail.com>
2023-09-14 22:39:40 +0100

    Implementando casos de uso - Caso de uso de check-in

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -27,7 +27,7 @@ describe('Authenticate Use Case', () => {
   })
 
   it('should not be able to authenticate with wrong email', async () => {
-    expect(() =>
+    await expect(() =>
       sut.execute({
         email: 'johndoe@example.com',
         password: '123456',
@@ -42,7 +42,7 @@ describe('Authenticate Use Case', () => {
       password_hash: await hash('123456', 6),
     })
 
-    expect(() =>
+    await expect(() =>
       sut.execute({
         email: 'johndoe@example.com',
         password: '123123',
